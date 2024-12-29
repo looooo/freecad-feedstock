dce0c8b9ef6c1a346463deb70839141088fae60a
Lorenz Lechner <sppedflyer@gmail.com>
2024-12-29 16:17:21 +0100

    use sha256 sum

--- recipe/increase_build_number.py
+++ recipe/increase_build_number.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 
 import re
+import requests
 
+
+# read meta.yaml file
 with open("recipe/meta.yaml", "r") as f:
     text = f.read()
 
+# increase build number
 build_number_old = int(re.search(r"(?<={% set build_number = )\d+", text)[0])
 build_number_new = build_number_old + 1
 print(f"set build_number from build_number {build_number_old} to {build_number_new}")
 text = re.sub(r"(?<={% set build_number = )\d+", str(build_number_new), text)
+
+# set sha256sum
+url = "https://github.com/FreeCAD/FreeCAD-Bundle/releases/download/weekly-builds/freecad_source_sha256sum.txt"
+response = requests.get(url)
+response.raise_for_status()
+sha256 = response.content.decode('utf-8')
+print(f"set sha256:  {sha256}")
+text = re.sub(r"(?<={% set sha256 = )\d+", str(sha256), text)
+
+# write meta.yaml file
 with open("recipe/meta.yaml", "w") as f:
     f.write(text)
